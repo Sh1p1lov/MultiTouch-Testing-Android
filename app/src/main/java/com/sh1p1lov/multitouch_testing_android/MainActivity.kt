8d76be8b12b757ae3fcff85b1a96b84559643aa6
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-03-17 01:42:45 +0300

    Added UI for displaying current touch count and max touch count

--- app/src/main/java/com/sh1p1lov/multitouch_testing_android/MainActivity.kt
+++ app/src/main/java/com/sh1p1lov/multitouch_testing_android/MainActivity.kt
@@ -2,10 +2,32 @@ package com.sh1p1lov.multitouch_testing_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.sh1p1lov.multitouch_testing_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+    private val viewModel by lazy { ViewModelProvider(this)[MainActivityViewModel::class.java] }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.multiTouchView.addOnCountTouchesUpdatedListener { touchCount ->
+            binding.tvTouchCount.text = touchCount.toString()
+            viewModel.tryUpdateMaxTouchCount(touchCount)
+        }
+
+        viewModel.maxTouchCountLiveData.observe(this) { maxTouchCount ->
+            binding.tvMaxTouchCount.text = maxTouchCount.toString()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
