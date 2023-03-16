c2b10da007abfdf8d9f3c2f8125dc9e41351dc8e
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-03-17 02:31:45 +0300

    Implementation displaying content edge-to-edge

--- app/src/main/java/com/sh1p1lov/multitouch_testing_android/MainActivity.kt
+++ app/src/main/java/com/sh1p1lov/multitouch_testing_android/MainActivity.kt
@@ -2,6 +2,12 @@ package com.sh1p1lov.multitouch_testing_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.ViewModelProvider
 import com.sh1p1lov.multitouch_testing_android.databinding.ActivityMainBinding
 
@@ -13,8 +19,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.tvMaxTouchCount) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            Log.d("MainActivityLog", insets.top.toString())
+            view.updateLayoutParams<MarginLayoutParams> { topMargin = insets.top }
+            WindowInsetsCompat.CONSUMED
+        }
 
         binding.multiTouchView.addOnCountTouchesUpdatedListener { touchCount ->
             binding.tvTouchCount.text = touchCount.toString()
